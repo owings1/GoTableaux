097ef85fb1decd7a1d6b101afb973b0e046c1f0a
Douglas Owings <owings1@gmail.com>
2012-01-22 15:39:03 -0800

    Added ManyValuedNode interface

---
+++ Logic/ProofSystem/Tableaux/Node/ManyValuedNode.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Defines the ManyValuedNode interface.
+ * @package Tableaux
+ * @author Douglas Owings
+ */
+
+/**
+ * Signifies a many-valued tableau node that has a designation marker.
+ * @package Tableaux
+ * @author Douglas Owings
+ */
+interface ManyValuedNode
+{
+	/**
+	 * Returns whether the node is designated.
+	 *
+	 * @return boolean Whether the node is designated.
+	 */
+	public function isDesignated();
+}
