a23fbbd5156c444898d21394f9d57532d310ea57
Douglas Owings <doug@dougowings.net>
2012-11-05 20:57:55 -0800

    adds listener file

---
+++ Source/ProofSystem/TableauxSystem/BranchEventListener.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * GoTableaux. A multi-logic tableaux generator.
+ * Copyright (C) 2012  Douglas Owings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY. 
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/agpl-3.0.html>.
+ */
+/**
+ * Defines the BranchRule interface.
+ * @package GoTableaux
+ */
+
+namespace GoTableaux\ProofSystem\TableauxSystem;
+
+use \GoTableaux\Logic as Logic;
+use \GoTableaux\Utilities as Utilities;
+
+class BranchEventListener
+{
+	public function afterAddNode()
+	{
+		
+	}
+	
+}
